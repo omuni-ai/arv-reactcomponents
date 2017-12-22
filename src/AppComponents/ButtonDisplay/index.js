c106f13ed265eff7f94725caa6026ad17daa867e
jessel.john <jessel.john@arvindinternet.com>
2017-12-22 15:31:22 +0530

    Styleguide updated

--- src/AppComponents/ButtonDisplay/index.js
+++ src/AppComponents/ButtonDisplay/index.js
@@ -10,31 +10,29 @@ function ButtonDisplay() {
           <h2>Color Variants</h2>
           <Button>Normal</Button>
           <span>&nbsp;</span>
-          <Button className="nwc-btn-primary">Primary</Button>
+          <Button className="nwc-btn-primary">.nwc-btn-primary</Button>
           <span>&nbsp;</span>
-          <Button className="nwc-btn-secondary">Secondary</Button>
+          <Button className="nwc-btn-secondary">.nwc-btn-secondary</Button>
           <span>&nbsp;</span>
-          <Button className="nwc-btn-success">Success</Button>
+          <Button className="nwc-btn-success">.nwc-btn-success</Button>
           <span>&nbsp;</span>
-          <Button className="nwc-btn-danger">Danger</Button>
+          <Button className="nwc-btn-danger">.nwc-btn-danger</Button>
         </GridColumn>
         <GridColumn className="nwc-grid-col-sm-12 nw-block nw-block-white">
           <h2>Size Variants</h2>
-          <Button className="nwc-btn-primary nwc-btn-xxl">Button xxl</Button>
+          <Button className="nwc-btn-xxl">.nwc-btn-xxl</Button>
           <span>&nbsp;</span>
-          <Button className="nwc-btn-primary nwc-btn-xl">Button xl</Button>
+          <Button className="nwc-btn-xl">.nwc-btn-xl</Button>
           <span>&nbsp;</span>
-          <Button className="nwc-btn-primary nwc-btn-lg">Button lg</Button>
+          <Button className="nwc-btn-lg">.nwc-btn-lg</Button>
           <span>&nbsp;</span>
-          <Button className="nwc-btn-primary nwc-btn-normal">
-            Button normal
-          </Button>
+          <Button className="nwc-btn-normal">.nwc-btn-normal</Button>
           <span>&nbsp;</span>
-          <Button className="nwc-btn-primary nwc-btn-sm">Button normal</Button>
+          <Button className="nwc-btn-sm">.nwc-btn-sm</Button>
           <span>&nbsp;</span>
-          <Button className="nwc-btn-primary nwc-btn-xs">Button normal</Button>
+          <Button className="nwc-btn-xs">.nwc-btn-xs</Button>
           <span>&nbsp;</span>
-          <Button className="nwc-btn-primary nwc-btn-xxs">Button normal</Button>
+          <Button className="nwc-btn-xxs">.nwc-btn-xxs</Button>
         </GridColumn>
       </GridRow>
     </GridColumn>
