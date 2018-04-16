f6f6b22de975e1ed437431a1e16ee6a89da3e6f3
jessel.john <jessel.john@arvindinternet.com>
2018-04-16 19:37:40 +0530

    Minor change

--- src/components/LazyImg/test.component.js
+++ src/components/LazyImg/test.component.js
@@ -1,20 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
 // Import common components
 import { LazyImg } from "../../components";
 
-class LazyImgExample extends Component {
-  render() {
-    return (
-      <LazyImg
-        index={0}
-        className="nw-carouselexample-img"
-        src="https://static.nnnow.com/client/assets/images/nnnow-logo-beta_1.png"
-        offset={300}
-        onWinLoad
-      />
-    );
-  }
-}
+const LazyImgExample = () => (
+  <LazyImg
+    index={0}
+    className="nw-carouselexample-img"
+    src="https://static.nnnow.com/client/assets/images/nnnow-logo-beta_1.png"
+    offset={300}
+    onWinLoad
+  />
+);
 
 export default LazyImgExample;
