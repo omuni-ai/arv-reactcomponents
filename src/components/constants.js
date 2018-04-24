08d5c46b9ca138e90998d9fd7e5add2f02c5468c
jessel.john <jessel.john@arvindinternet.com>
2018-04-24 14:07:42 +0530

    Prettyfied code

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,14 +1,15 @@
 /* eslint-disable */
 
 export const isMobile = window.innerWidth <= 768;
-export const localStorage = "localStorage" in window && window.localStorage || null;
-
-
+export const localStorage =
+  ("localStorage" in window && window.localStorage) || null;
 
 export const vendorsPrefixes = ["ms", "moz", "webkit", "o"];
-export const requestAnimationFrameVendor = vendorsPrefixes.find(prefix => window[`${prefix}RequestAnimationFrame`]);
+export const requestAnimationFrameVendor = vendorsPrefixes.find(
+  prefix => window[`${prefix}RequestAnimationFrame`],
+);
 export const cancelAnimationFrameVendor = vendorsPrefixes.find(
-prefix =>
-  window[`${prefix}CancelAnimationFrame`] ||
-  window[`${prefix}CancelRequestAnimationFrame`],
+  prefix =>
+    window[`${prefix}CancelAnimationFrame`] ||
+    window[`${prefix}CancelRequestAnimationFrame`],
 );
