463ea8345eed271ef1a2f06b585433e1371ce8ad
Priyanka <you@example.com>
2018-09-04 13:14:50 +0530

    added syntax-highlighter for code showing in modal

--- src/AppComponents/ShowCode/index.js
+++ src/AppComponents/ShowCode/index.js
@@ -1,6 +1,13 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
+import SyntaxHighlighter, {
+  registerLanguage,
+} from "react-syntax-highlighter/prism-light";
+import jsx from "react-syntax-highlighter/languages/prism/jsx";
+import prism from "react-syntax-highlighter/styles/prism/prism";
 import { Button, Modal, Toastr } from "../../components";
+
+registerLanguage("jsx", jsx);
 
 const fetchUrl = `/api/getComponentData`;
 class ShowCode extends PureComponent {
@@ -62,11 +69,9 @@ class ShowCode extends PureComponent {
             this.showCodeMsg(false);
           }}
         >
-          <pre>
-            <code>
-              <div>{`${code}`}</div>
-            </code>
-          </pre>
+          <SyntaxHighlighter language="javascript" style={prism}>
+            {`${code}`}
+          </SyntaxHighlighter>
         </Modal>
       </div>
     );
