caa7ead49a38312ae13e77a4b99933722441deb1
dev <dharmadev.gupta@arvindinternet.con>
2018-09-06 16:27:12 +0530

    setting new server for serving html

--- src/server.js
+++ src/server.js
@@ -1,29 +1,22 @@
 /* eslint-disable */
 require("babel-register");
 const express = require("express");
+const path = require("path");
 const compression = require("compression");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
-
-const app = express();
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = process.env.BABEL_ENV || "development";
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-//TODO: need to check little bit more for request object
-// if (process.env.NODE_ENV !== "production") {
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+const app = express();
+
 process.on("unhandledRejection", err => {
   console.log(err.stack);
   console.log("got unhandledRejection error ", err);
 });
 // Ensure environment variables are read.
-require("../../config/env");
-
-const fs = require("fs");
-
-// Tools like Cloud9 rely on this.
+require("../config/env");
 const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
 const HOST = process.env.HOST || "0.0.0.0";
 
@@ -33,19 +26,22 @@ const HOST = process.env.HOST || "0.0.0.0";
     app.use(compression());
     app.use(morgan("dev"));
     app.use(bodyParser.json({ limit: "1mb" }));
-    app.set("view engine", "html");
+    app.set('view engine', 'html');
+    console.log(__dirname + '/views');
+
+    app.set('views', __dirname + '/views');
+    app.use(express.static(path.join(__dirname, '../build')));
+    // app.use(express.static("../build"));
+    app.get("/", function(req, res) {
+      return res.status(200).render("../build");
+    });
 
     // Launch express server for listening.
     app.listen(port, HOST, err => {
       if (err) {
         return console.log(err);
-        //  https://github.com/ButterCMS/ssr-cra-async/blob/master/server/index.js
       }
-      console.log(
-        chalk.cyan(
-          `Starting the express server in  ${process.env.NODE_ENV} mode ...\n`,
-        ),
-      );
+      console.log(`Starting the express server in  ${process.env.NODE_ENV} mode ...\n`);
     });
 
     ["SIGINT", "SIGTERM"].forEach(sig => {
