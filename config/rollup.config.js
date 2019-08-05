b53c80bcbcefabd3eb3ce71196f4bc38bf2d9fa2
Prince Kumar <princekumar@arvindinternetlocaladmins-iMac.local>
2019-08-05 15:48:04 +0530

    SITE-1434 added rollup config files

---
+++ config/rollup.config.js
@@ -0,0 +1,69 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
+import replace from 'rollup-plugin-replace';
+
+import copyFiles from "./rollup.copyfiles.plugin.js"
+
+var config = [{
+  input: 'src/components/index.js',
+  external: ['react', 'prop-types'],
+  output: [{
+    file: 'bundle/cjs/main.js',
+    format: 'cjs',
+    sourcemap: true
+  }],
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        'src/components/constants.js': ['windowRequestAnimationFrame', 'vendorRequestAnimationFrame']
+      }
+    }),
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      "presets": [
+        ["env", {
+          "modules": false
+        }],
+        "es2017",
+        "stage-0",
+        "react"
+      ],
+      plugins: ['external-helpers']
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    copyFiles({
+      input: 'src/components',
+      output: 'bundle/components',
+      matches: /.*\.scss$/
+    })
+  ]
+}, {
+  input: 'bundle/cjs/main.js',
+  output: [{
+    file: 'bundle/cjs/main.min.js',
+    format: 'cjs'
+  }],
+  plugins: [
+    uglify({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    })
+  ]
+}];
+
+export default config;
